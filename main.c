bf61fae6c3cd1e8b0abd4fdee40f89fff10e79c6
Jason Frame <jason@onehackoranother.com>
2015-12-03 22:45:32 +0000

    represent AST with struct pointers rather instead of an integer-indexed AST pool. might change back later.

--- main.c
+++ main.c
@@ -10,28 +10,22 @@
  */
 
 enum {
-    OP_PRINT    = (1 << 26),
-    OP_HALT     = (2 << 26),
-    OP_ADD      = (3 << 26),
-    OP_LOADK    = (4 << 26),
-    OP_COPY     = (5 << 26)
-};
-
-enum {
-    AST_STMTS,
-    AST_PRINT,
-    AST_ASSIGN,
-    AST_LITERAL,
-    AST_ADD,
-    AST_IDENT
-};
-
-enum {
     T_NIL,
     T_AST,
     T_INT,
     T_IDENT
 };
+
+enum {
+    AST_STMTS       = 0x01,
+    AST_PRINT       = 0x02,
+    AST_IDENT       = 0x03,
+
+    AST_ADD         = 0x81,
+    AST_ASSIGN      = 0x82
+};
+
+typedef struct ast_node ast_node_t;
 
 typedef struct val val_t;
 
@@ -39,18 +33,40 @@ struct val {
     int type;
     union {
         int ival;
+        ast_node_t *ast;
     };
 };
 
-typedef struct {
+struct ast_node {
     int type;
-    val_t o1;
-    val_t o2;
-    val_t o3;
-} ast_node_t;
+};
 
-ast_node_t ast_pool[32768];
-int next_free_ast_node = 1;
+typedef struct ast_print {
+    ast_node_t base;
+    val_t exp;
+} ast_print_t;
+
+typedef struct ast_stmts ast_stmts_t;
+
+struct ast_stmts {
+    ast_node_t base;
+    val_t stmt;
+    val_t next;
+};
+
+typedef struct ast_binop {
+    ast_node_t base;
+    val_t l;
+    val_t r;
+} ast_binop_t;
+
+enum {
+    OP_PRINT    = (1 << 26),
+    OP_HALT     = (2 << 26),
+    OP_ADD      = (3 << 26),
+    OP_LOADK    = (4 << 26),
+    OP_COPY     = (5 << 26)
+};
 
 val_t mk_nil() {
     val_t out = { .type = T_NIL };
@@ -71,39 +87,30 @@ val_t mk_ident(int id) {
     return out;
 }
 
-val_t mk_ast1(int type, val_t n1) {
-    val_t out;
-    out.type = T_AST;
-    int node_ix = out.ival = next_free_ast_node++;
-    ast_pool[node_ix].type = type;
-    ast_pool[node_ix].o1 = n1;
-    return out;
+#define ALLOC_AST(struct_type, tag) \
+    struct_type *node = malloc(sizeof(struct_type)); \
+    ((ast_node_t*)node)->type = tag; \
+    val_t val = { .type = T_AST, .ast = (ast_node_t*) node }
+
+val_t mk_ast_stmt(val_t stmt, val_t next) {
+    ALLOC_AST(ast_stmts_t, AST_STMTS);
+    node->stmt = stmt;
+    node->next = next;
+    return val;
 }
 
-val_t mk_ast2(int type, val_t n1, val_t n2) {
-    val_t out;
-    out.type = T_AST;
-    int node_ix = out.ival = next_free_ast_node++;
-    ast_pool[node_ix].type = type;
-    ast_pool[node_ix].o1 = n1;
-    ast_pool[node_ix].o2 = n2;
-    return out;
+val_t mk_ast_print(val_t exp) {
+    ALLOC_AST(ast_print_t, AST_PRINT);
+    node->exp = exp;
+    return val;
 }
 
-val_t mk_ast3(int type, val_t n1, val_t n2, val_t n3) {
-    val_t out;
-    out.type = T_AST;
-    int node_ix = out.ival = next_free_ast_node++;
-    ast_pool[node_ix].type = type;
-    ast_pool[node_ix].o1 = n1;
-    ast_pool[node_ix].o2 = n2;
-    ast_pool[node_ix].o3 = n3;
-    return out;
+val_t mk_ast_binop(int type, val_t l, val_t r) {
+    ALLOC_AST(ast_binop_t, type);
+    node->l = l;
+    node->r = r;
+    return val;
 }
-
-#define MK_AST1(type, n1) mk_ast1(type, n1)
-#define MK_AST2(type, n1, n2) mk_ast2(type, n1, n2)
-#define MK_AST3(type, n1, n2, n3) mk_ast3(type, n1, n2, n3)
 
 typedef uint32_t inst_t;
 
@@ -115,78 +122,71 @@ typedef struct {
     int reg;
 } code_t;
 
-int compile_exp(int ast_node, code_t *code);
-int compile_assign(int ast_node, code_t *code);
-void compile_print(int ast_node, code_t *code);
-int compile_literal(int ast_node, code_t *code);
-int compile_add(int ast_node, code_t *code);
+int nil_p(val_t v) {
+    return v.type == T_NIL;
+}
 
-#define AST_NEXT(n) ast_node = ast_pool[ast_node].o##n.ival
+int ast_type(val_t v) {
+    return v.ast->type;
+}
+
+int compile_exp(val_t exp, code_t *code);
+void compile_print(val_t exp, code_t *co);
 
 // Register allocation:
 // https://en.wikipedia.org/wiki/Sethi%E2%80%93Ullman_algorithm
 // https://lambda.uta.edu/cse5317/fall02/notes/node40.html
 // http://www.christianwimmer.at/Publications/Wimmer10a/Wimmer10a.pdf
 
-void compile_statements(int ast_node, code_t *code) {
-    while (ast_node) {
-        switch (ast_pool[ast_node].type) {
+void compile_statements(val_t stmt, code_t *code) {
+    while (!nil_p(stmt)) {
+        val_t subj = ((ast_stmts_t*)stmt.ast)->stmt;
+        switch (ast_type(subj)) {
             case AST_PRINT:
-                compile_print(ast_node, code);
-                AST_NEXT(2);
-                break;
-            case AST_ASSIGN:
-                compile_assign(ast_node, code);
-                AST_NEXT(3);
+                compile_print(subj, code);
                 break;
             default:
-                printf("unknown node type: %d\n", ast_pool[ast_node].type);
+                compile_exp(subj, code);
                 break;
         }
+        stmt = ((ast_stmts_t*)stmt.ast)->next;
     }
 }
 
-int compile_exp(int ast_node, code_t *co) {
-    printf("compile exp: %d\n", ast_pool[ast_node].type);
-    switch (ast_pool[ast_node].type) {
-        case AST_LITERAL:
-            return compile_literal(ast_node, co);
-        case AST_ADD:
-            return compile_add(ast_node, co);
-        case AST_IDENT:
-            return ast_pool[ast_node].o1.ival;
-        default:
-            printf("unknown expression type: %d\n", ast_pool[ast_node].type);
-            return 0;
+int compile_exp(val_t val, code_t *co) {
+    // printf("compile exp: %d\n", ast_type(val));
+    if (val.type == T_IDENT) {
+        return val.ival;
+    } else if (val.type == T_INT) {
+        int constant = co->ki++;
+        co->constants[constant] = val;
+        int dst = co->reg++;
+        co->code[co->pi++] = OP_LOADK | (dst << 16) | constant;
+        return dst;
+    } else if (val.type == T_AST) {
+        if (val.ast->type & 0x80) {
+            ast_binop_t *op = (ast_binop_t*)val.ast;
+            if (op->base.type == AST_ADD) {
+                int lreg = compile_exp(op->l, co);
+                int rreg = compile_exp(op->r, co);
+                int oreg = co->reg++;
+                co->code[co->pi++] = OP_ADD | (oreg << 16) | (lreg << 8) | rreg;
+                return oreg;
+            } else if (op->base.type == AST_ASSIGN) {
+                int dst = op->l.ival;
+                int src = compile_exp(op->r, co);
+                co->code[co->pi++] = OP_COPY | (dst << 16) | src;
+                return dst;
+            }
+        }
+        printf("unknown AST type for expression: %d\n", val.ast->type);
     }
+    return -1;
 }
 
-int compile_assign(int ast_node, code_t *co) {
-    int dst = ast_pool[ast_pool[ast_node].o1.ival].o1.ival;
-    int src = compile_exp(ast_pool[ast_node].o2.ival, co);
-    co->code[co->pi++] = OP_COPY | (dst << 16) | src;
-    return dst;
-}
-
-void compile_print(int ast_node, code_t *co) {
-    int reg = compile_exp(ast_pool[ast_node].o1.ival, co);
+void compile_print(val_t exp, code_t *co) {
+    int reg = compile_exp(((ast_print_t*)exp.ast)->exp, co);
     co->code[co->pi++] = OP_PRINT | reg;
-}
-
-int compile_literal(int ast_node, code_t *co) {
-    int constant = co->ki++;
-    co->constants[constant] = ast_pool[ast_node].o1;
-    int dst = co->reg++;
-    co->code[co->pi++] = OP_LOADK | (dst << 16) | constant;
-    return dst;
-}
-
-int compile_add(int ast_node, code_t *co) {
-    int lreg = compile_exp(ast_pool[ast_node].o1.ival, co);
-    int rreg = compile_exp(ast_pool[ast_node].o2.ival, co);
-    int oreg = co->reg++;
-    co->code[co->pi++] = OP_ADD | (oreg << 16) | (lreg << 8) | rreg;
-    return oreg;
 }
 
 code_t* compile(val_t program, int nlocals) {
@@ -197,7 +197,7 @@ code_t* compile(val_t program, int nlocals) {
     co->ki = 0;
     co->reg = nlocals;
 
-    compile_statements(ast_pool[program.ival].o1.ival, co);
+    compile_statements(program, co);
 
     co->code[co->pi++] = OP_HALT;
 
@@ -254,20 +254,19 @@ int main(int argc, char *argv[]) {
     // b = a + 5
     // print b
 
-    val_t program = MK_AST1(AST_STMTS,
-        MK_AST3(AST_ASSIGN,
-            MK_AST1(AST_IDENT, mk_ident(0)),
-            MK_AST1(AST_LITERAL, mk_int(10)),
-            MK_AST3(AST_ASSIGN,
-                MK_AST1(AST_IDENT, mk_ident(1)),
-                MK_AST2(AST_ADD,
-                    MK_AST1(AST_IDENT, mk_ident(0)),
-                    MK_AST1(AST_LITERAL, mk_int(5))
-                ),
-                MK_AST2(AST_PRINT,
-                    MK_AST1(AST_IDENT, mk_ident(1)),
-                    mk_nil()
+    val_t program = mk_ast_stmt(
+        mk_ast_binop(AST_ASSIGN, mk_ident(0), mk_int(10)),
+        mk_ast_stmt(
+            mk_ast_binop(AST_ASSIGN,
+                mk_ident(1),
+                mk_ast_binop(AST_ADD,
+                    mk_ident(0),
+                    mk_int(5)
                 )
+            ),
+            mk_ast_stmt(
+                mk_ast_print(mk_ident(1)),
+                mk_nil()
             )
         )
     );
