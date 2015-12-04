15f65fa51c55fec76ecf575f53b135dce32c0177
Jason Frame <jason@onehackoranother.com>
2015-12-04 23:16:47 +0000

    foreign function calling

--- main.c
+++ main.c
@@ -13,13 +13,15 @@ enum {
     T_NIL,
     T_AST,
     T_INT,
-    T_IDENT
+    T_IDENT,
+    T_FOREIGN_FN
 };
 
 enum {
     AST_STMTS       = 0x01,
     AST_PRINT       = 0x02,
     AST_IDENT       = 0x03,
+    AST_CALL        = 0x04,
 
     AST_ADD         = 0x81,
     AST_SUB         = 0x82,
@@ -32,17 +34,26 @@ typedef struct ast_node ast_node_t;
 
 typedef struct val val_t;
 
+typedef val_t (*foreign_fn_f)(val_t *args, int nargs);
+
 struct val {
     int type;
     union {
         int ival;
         ast_node_t *ast;
+        foreign_fn_f fn;
     };
 };
 
 struct ast_node {
     int type;
 };
+
+typedef struct ast_call {
+    ast_node_t base;
+    val_t callee;
+    val_t args;
+} ast_call_t;
 
 typedef struct ast_print {
     ast_node_t base;
@@ -69,7 +80,8 @@ enum {
     OP_ADD      = (3 << 26),
     OP_SUB      = (4 << 26),
     OP_LOADK    = (5 << 26),
-    OP_COPY     = (6 << 26)
+    OP_COPY     = (6 << 26),
+    OP_CALL     = (7 << 26)
 };
 
 val_t mk_nil() {
@@ -103,6 +115,13 @@ val_t mk_ast_stmt(val_t stmt, val_t next) {
     return val;
 }
 
+val_t mk_ast_call(val_t callee, val_t args) {
+    ALLOC_AST(ast_call_t, AST_CALL);
+    node->callee = callee;
+    node->args = args;
+    return val;
+}
+
 val_t mk_ast_print(val_t exp) {
     ALLOC_AST(ast_print_t, AST_PRINT);
     node->exp = exp;
@@ -126,12 +145,26 @@ typedef struct {
     int reg;
 } code_t;
 
+val_t mul(val_t *args, int nargs) {
+    printf("mul: %d %d %d\n", args[0].ival, args[1].ival, args[2].ival);
+    return mk_int(args[0].ival * args[1].ival * args[2].ival);
+}
+
 int nil_p(val_t v) {
     return v.type == T_NIL;
 }
 
 int ast_type(val_t v) {
     return v.ast->type;
+}
+
+int ast_list_len(val_t v) {
+    int len = 0;
+    while (!nil_p(v)) {
+        len++;
+        v = ((ast_stmts_t*)v.ast)->next;
+    }
+    return len;
 }
 
 int compile_exp(val_t exp, code_t *code);
@@ -189,6 +222,25 @@ int compile_exp(val_t val, code_t *co) {
                 co->code[co->pi++] = OP_COPY | (dst << 16) | src;
                 return dst;
             }
+        } else if (val.ast->type == AST_CALL) {
+            ast_call_t *call = (ast_call_t*)val.ast;
+            int nargs = ast_list_len(call->args);
+            int r_callee = co->reg++;
+            int r_argbase = co->reg;
+            co->reg += nargs;
+            int r_callee_val = compile_exp(call->callee, co);
+            co->code[co->pi++] = OP_COPY | (r_callee << 16) | r_callee_val;
+            val_t thisarg = call->args;
+            int argix = 0;
+            while (!nil_p(thisarg)) {
+                int r_arg = compile_exp(((ast_stmts_t*)thisarg.ast)->stmt, co);
+                co->code[co->pi++] = OP_COPY | ((r_argbase + argix) << 16) | r_arg;
+                argix++;
+                thisarg = ((ast_stmts_t*)thisarg.ast)->next;
+            }
+            int r_res = co->reg++;
+            co->code[co->pi++] = OP_CALL | (r_callee << 16) | (nargs << 8) | r_res;
+            return r_res;
         }
         printf("unknown AST type for expression: %d\n", val.ast->type);
     }
@@ -218,6 +270,10 @@ code_t* compile(val_t program, int nlocals) {
 void run(code_t *co) {
     int ip = 0;
     val_t reg[128];
+
+    reg[2].type = T_FOREIGN_FN;
+    reg[2].fn = mul;
+
     while (1) {
         inst_t op = co->code[ip++];
         printf("op: 0x%x\n", op);
@@ -258,6 +314,14 @@ void run(code_t *co) {
                     reg[rd] = reg[rs];
                 }
                 break;
+            case OP_CALL:
+                {
+                    int base = (op >> 16) & 0xFF;
+                    int nargs = (op >> 8) & 0xFF;
+                    int result = op & 0xFF;
+                    reg[result] = reg[base].fn(&reg[base+1], nargs);
+                }
+                break;
             case OP_HALT:
                 {
                     printf("execution terminated\n");    
@@ -278,19 +342,33 @@ int main(int argc, char *argv[]) {
         mk_ast_stmt(
             mk_ast_binop(AST_ASSIGN,
                 mk_ident(1),
-                mk_ast_binop(AST_SUB,
+                mk_ast_binop(AST_ADD,
                     mk_ident(0),
                     mk_int(5)
                 )
             ),
             mk_ast_stmt(
-                mk_ast_print(mk_ident(1)),
+                mk_ast_print(
+                    mk_ast_call(
+                        mk_ident(2),
+                        mk_ast_stmt(
+                            mk_int(2),
+                            mk_ast_stmt(
+                                mk_ident(0),
+                                mk_ast_stmt(
+                                    mk_ident(1),
+                                    mk_nil()
+                                )
+                            )
+                        )
+                    )
+                ),
                 mk_nil()
             )
         )
     );
 
-    code_t *code = compile(program, 2);
+    code_t *code = compile(program, 3);
 
     run(code);
 
