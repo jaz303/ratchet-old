7e90264732139f0784d25c02b1e11883d5fa136e
Jason Frame <jason@onehackoranother.com>
2015-12-07 01:29:44 +0000

    tabs -> spaces

--- lexer.c
+++ lexer.c
@@ -2,31 +2,31 @@
 #include "util.inc.c"
 
 enum {
-	TOK_IDENT = 1,
-	TOK_ASSIGN,
-	TOK_WHILE,
-	TOK_LT,
-	TOK_INT,
-	TOK_LBRACE,
-	TOK_RBRACE,
-	TOK_LPAREN,
-	TOK_RPAREN,
-	TOK_PLUS,
+    TOK_IDENT = 1,
+    TOK_ASSIGN,
+    TOK_WHILE,
+    TOK_LT,
+    TOK_INT,
+    TOK_LBRACE,
+    TOK_RBRACE,
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_PLUS,
 
-	TOK_META = 1000,
-	TOK_EOF,
-	TOK_ERROR
+    TOK_META = 1000,
+    TOK_EOF,
+    TOK_ERROR
 };
 
 typedef struct rt_lexer {
-	char *text;
-	size_t len;
-	size_t pos;
-	int state;
-	int tok_start;
-	int tok_len;
-	char *tok;
-	char *error;
+    char *text;
+    size_t len;
+    size_t pos;
+    int state;
+    int tok_start;
+    int tok_len;
+    char *tok;
+    char *error;
 } rt_lexer_t;
 
 #define MARK() l->tok_start = l->pos; l->tok = &(l->text[l->pos])
@@ -37,101 +37,101 @@ typedef struct rt_lexer {
 #define LEN() (l->tok_len)
 #define TEXTEQ(str) rt_lexer_texteq(l, str)
 #define ERROR(msg) \
-	l->error = msg; \
-	return TOK_ERROR
+    l->error = msg; \
+    return TOK_ERROR
 
 int space_p(char c) {
-	return c == ' ' || c == '\r' || c == '\n' || c == '\t';
+    return c == ' ' || c == '\r' || c == '\n' || c == '\t';
 }
 
 int digit_p(char c) {
-	return c >= '0' && c <= '9';
+    return c >= '0' && c <= '9';
 }
 
 int ident_start_p(char c) {
-	return (c >= 'A' && c <= 'Z')
-			|| (c >= 'a' && c <= 'z')
-			|| (c == '_');
+    return (c >= 'A' && c <= 'Z')
+            || (c >= 'a' && c <= 'z')
+            || (c == '_');
 }
 
 int ident_rest_p(char c) {
-	return ident_start_p(c) || digit_p(c);
+    return ident_start_p(c) || digit_p(c);
 }
 
 int rt_lexer_texteq(rt_lexer_t *l, const char *text) {
-	int p = 0;
-	while (p < l->tok_len && text[p]) {
-		if (l->tok[p] != text[p]) return 0;
-		p++;
-	}
-	return p == l->tok_len && text[p] == 0;
+    int p = 0;
+    while (p < l->tok_len && text[p]) {
+        if (l->tok[p] != text[p]) return 0;
+        p++;
+    }
+    return p == l->tok_len && text[p] == 0;
 }
 
 void rt_lexer_init(rt_lexer_t *lexer, char *text) {
-	lexer->text = text;
-	lexer->pos = 0;
-	lexer->tok_start = 0;
-	lexer->tok_len = 0;
-	lexer->tok = NULL;
-	lexer->error = NULL;
+    lexer->text = text;
+    lexer->pos = 0;
+    lexer->tok_start = 0;
+    lexer->tok_len = 0;
+    lexer->tok = NULL;
+    lexer->error = NULL;
 }
 
 int rt_lexer_next(rt_lexer_t *l) {
-	if (l->error) {
-		return TOK_ERROR;
-	}
-	while (space_p(l->text[l->pos])) {
-		l->pos++;
-	}
-	switch (CURR()) {
-		case 0: EMIT(TOK_EOF);
-		case '(': NEXT(); EMIT(TOK_LPAREN);
-		case ')': NEXT(); EMIT(TOK_RPAREN);
-		case '{': NEXT(); EMIT(TOK_LBRACE);
-		case '}': NEXT(); EMIT(TOK_RBRACE);
-		case '<': NEXT(); EMIT(TOK_LT);
-		case '+': NEXT(); EMIT(TOK_PLUS);
-		case ':':
-			NEXT();
-			if (CURR() == '=') {
-				NEXT();
-				EMIT(TOK_ASSIGN);
-			} else {
-				ERROR("expected: '='");
-			}
-		default:
-			if (ident_start_p(CURR())) {
-				MARK(); NEXT();
-				while (ident_rest_p(CURR())) {
-					NEXT();
-				}
-				END();
-				if (TEXTEQ("while")) {
-					return TOK_WHILE;
-				} else {
-					return TOK_IDENT;
-				}
-			} else if (digit_p(CURR())) {
-				MARK(); NEXT();
-				while (digit_p(CURR())) {
-					NEXT();
-				}
-				END();
-				EMIT(TOK_INT);
-			} else {
-				ERROR("unexpected character in input");
-			}
-	}
+    if (l->error) {
+        return TOK_ERROR;
+    }
+    while (space_p(l->text[l->pos])) {
+        l->pos++;
+    }
+    switch (CURR()) {
+        case 0: EMIT(TOK_EOF);
+        case '(': NEXT(); EMIT(TOK_LPAREN);
+        case ')': NEXT(); EMIT(TOK_RPAREN);
+        case '{': NEXT(); EMIT(TOK_LBRACE);
+        case '}': NEXT(); EMIT(TOK_RBRACE);
+        case '<': NEXT(); EMIT(TOK_LT);
+        case '+': NEXT(); EMIT(TOK_PLUS);
+        case ':':
+            NEXT();
+            if (CURR() == '=') {
+                NEXT();
+                EMIT(TOK_ASSIGN);
+            } else {
+                ERROR("expected: '='");
+            }
+        default:
+            if (ident_start_p(CURR())) {
+                MARK(); NEXT();
+                while (ident_rest_p(CURR())) {
+                    NEXT();
+                }
+                END();
+                if (TEXTEQ("while")) {
+                    return TOK_WHILE;
+                } else {
+                    return TOK_IDENT;
+                }
+            } else if (digit_p(CURR())) {
+                MARK(); NEXT();
+                while (digit_p(CURR())) {
+                    NEXT();
+                }
+                END();
+                EMIT(TOK_INT);
+            } else {
+                ERROR("unexpected character in input");
+            }
+    }
 }
 
 int main(int argc, char *argv[]) {
-	char *source = readfile(argv[1]);
-	rt_lexer_t lexer;
-	rt_lexer_init(&lexer, source);
-	int tok;
-	while ((tok = rt_lexer_next(&lexer)) < TOK_META) {
-		printf("%d\n", tok);
-	}
+    char *source = readfile(argv[1]);
+    rt_lexer_t lexer;
+    rt_lexer_init(&lexer, source);
+    int tok;
+    while ((tok = rt_lexer_next(&lexer)) < TOK_META) {
+        printf("%d\n", tok);
+    }
 }
 
 #undef MARK
